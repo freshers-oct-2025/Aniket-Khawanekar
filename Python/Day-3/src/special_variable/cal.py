2e92b0e07f89e901a4cf8c70adb7d818428d3c19
aniketk05 <aniket.khawanekar@thinkitive.com>
2025-11-11 20:10:30 +0530

    Python day 5 tasks Completed

--- Python/Day-3/src/special_variable/cal.py
+++ Python/Day-3/src/special_variable/cal.py
@@ -9,5 +9,6 @@ def main():
     add()
     subtract()
 
-# if __name__ == "__main__":
-main()
+
+if __name__ == "__main__":
+    main()
---
+++ Python/test.py
@@ -0,0 +1,7 @@
+str="Hello"
+
+# if(str[-1]=='a'or str[-1]=='e' or str[-1]=='i' or str[-1]=='o' or str[-1]=='u'):
+#     print("string end vowels")
+# else:
+#     print("string not end vowels")
+    
