1f2406f44ceb98d3d32dc90f65a2b639e5092bc8
aniketk05 <aniket.khawanekar@thinkitive.com>
2025-11-21 14:23:28 +0530

    extends ollama implementation

--- Python/Day-9/.ollama/main.py
+++ Python/Day-9/.ollama/main.py
@@ -1,13 +1,76 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from models import  SOAPRequest
+from models import Query
+from models import ConversationRequest
 import requests
+import json
 
 app = FastAPI()
 
-class Query(BaseModel):
-    prompt: str
 
 OLLAMA_URL = "http://localhost:11434/api/generate"
+
+@app.post("/soap")
+async def generate_soap_notes(req:SOAPRequest):
+    prompt = f"""
+Generate SOAP medical notes based on the patient conversation below.
+
+Patient Text:
+{req.patient_text}
+
+Return the output STRICTLY in JSON with this structure:
+
+{{
+"Subjective": "",
+"Objective": "",
+"Assessment": "",
+"Plan": ""
+}}
+"""
+
+    payload = {
+        "model": "qwen3:0.6b",
+        "prompt": prompt,
+        "stream": False
+    }
+
+    response = requests.post(OLLAMA_URL, json=payload)
+    data = response.json()
+
+    return {"soap_notes": data.get("response")}
+
+@app.post("/progress-notes")
+def generate_progress_notes(req: ConversationRequest):
+    conversation_text = "\n".join(req.messages)
+
+    prompt = f"""
+    You are a medical assistant. Generate Progress Notes based on this conversation:
+
+    {conversation_text}
+
+    Return output ONLY in JSON:
+
+    {{
+    "ConversationSummary": "",
+    "PatientStatements": "",
+    "ClinicianStatements": "",
+    "Assessment": "",
+    "TreatmentGiven": "",
+    "PatientResponse": "",
+    "NextSteps": ""
+    }}
+    """
+
+    payload = {
+        "model": "qwen3:0.6b",
+        "prompt": prompt,
+        "stream": False
+    }
+
+    r = requests.post(OLLAMA_URL, json=payload)
+    text = r.json().get("response", "")
+
+    return {"progress_notes": json.loads(text)}
 
 @app.post("/ask")
 async def ask(query: Query):
@@ -20,4 +83,6 @@ async def ask(query: Query):
     response = requests.post(OLLAMA_URL, json=payload)
     data = response.json()
 
-    return {"answer": data.get("response")}
+    soap = json.loads(data.get("response"))
+
+    return soap
